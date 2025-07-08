e17df9ca7128e0eefae13c6523bcd03ab1ee9ab3
Brolyte <85935641+Brolyte@users.noreply.github.com>
2025-07-08 13:12:27 +0200

    Add files via upload

---
+++ inc/languages/admin/config_fiche_personnage.lang.php
@@ -0,0 +1,40 @@
+<?php
+// Phrases du Panneau d'Admin - Sentences from the ACP
+
+ $l['fiche_personnage_name'] = 'Fiches de personnage';
+ $l['fiche_personnage_desc'] = 'Ce plugin permet de créer, mettre en page et modifier les fiches de personnage du forum.';
+ 
+ $l['fiche_personnage_settings_title'] = 'Paramètres des fiches de personnages';
+ $l['fiche_personnage_settings_desc'] = 'Configurer la mise en place des fiches de personnages';
+
+ $l['fiche_personnage_allowtomoderate_title'] = 'Groupes autorisés à modérer les fiches';
+ $l['fiche_personnage_allowtomoderate_desc'] = 'Sélectionnez le(s) groupe(s) autorisé(s) à modérer les fiches de personnages. <i>Pour sélectionner plusieurs groupes, maintenez Ctrl (ou Cmd sur Mac) enfoncé et cliquez sur les groupes souhaités.</i>';
+ $l['fiche_personnage_allowtopost_title'] = 'Groupes autorisés à créer une fiche';
+ $l['fiche_personnage_allowtopost_desc'] = 'Sélectionnez le(s) groupe(s) autorisé(s) à créer une fiche de personnage. <i>Pour sélectionner plusieurs groupes, maintenez Ctrl (ou Cmd sur Mac) enfoncé et cliquez sur les groupes souhaités.</i>';
+ $l['fiche_personnage_postlimit_title'] = 'Nombre de fiche(s) autorisé(es) par utilisateur.rice';
+ $l['fiche_personnage_postlimit_desc'] = 'Indiquez la limite autorisée de fiches par utilisateur.rice. <i>Laissez 0 pour un usage illimité.</i>';
+ $l['fiche_personnage_awaitinglist_forum_title'] = 'Forum des fiches en attente de validation';
+ $l['fiche_personnage_awaitinglist_forum_desc'] = 'Où publier les fiches à valider ou encore sous forme de brouillon? En choisissant un forum, le bouton « Nouveau » sera remplacé par le texte que vous choisirez et le lien sera automatiquement remplacé pour mener au formulaire de création de fiche de personnage.'; 
+ $l['fiche_personnage_approvedlist_forum_title'] = 'Forum des fiches validées';
+ $l['fiche_personnage_approvedlist_forum_desc'] = 'Choisissez un forum différent de celui dédié à la validation pour afficher uniquement les fiches validées (cela supprimera également le bouton « Nouveau sujet »). Sélectionnez « Aucun » pour afficher les fiches validées dans le même forum que celles en attente de validation.';
+ $l['fiche_personnage_button_custom_text_title'] = 'Texte du bouton « Nouveau »';
+ $l['fiche_personnage_button_custom_text_desc'] = 'Quel texte voulez-vous afficher sur le bouton pour créer une nouvelle fiche?'; 
+ $l['fiche_personnage_button_custom_css_title'] = 'CSS personnalisé pour le bouton de création';
+ $l['fiche_personnage_button_custom_css_desc'] = 'Si rempli, ce CSS <b>écrit sur une ligne</b> remplacera le style par défaut du bouton.';
+ $l['fiche_personnage_prefix_title'] = 'Préfixe des fiches';
+ $l['fiche_personnage_prefix_desc'] = 'Quel préfixe souhaitez-vous utiliser pour identifier les fiches? Laissez vide si vous ne souhaitez pas de préfixe.';
+ $l['fiche_personnage_icon_title'] = 'Icône des fiches';
+ $l['fiche_personnage_icon_desc'] = 'Quelle icône souhaitez-vous utiliser pour identifier les fiches? Laissez vide si vous ne souhaitez pas d’icône.';
+ $l['fiche_personnage_linkinprofile_title'] = 'Lien de la fiche dans le profil';
+ $l['fiche_personnage_linkinprofile_desc'] = 'Souhaitez-vous afficher le lien de la fiche du personnage dans son profil?';
+ $l['fiche_personnage_linkinmemberlist_title'] = 'Lien de la fiche dans la liste des membres';
+ $l['fiche_personnage_linkinmemberlist_desc'] = 'Souhaitez-vous afficher le lien de la fiche du personnage dans la liste des membres?';
+
+ $l['fiche_personnage_desinstallation_warning'] = 'Vous êtes sûr.e de vouloir désinstaller le plugin Fiches de Personnage? Ceci va supprimer toutes les fiches, pensez à faire une sauvegarde de vos tables avant d\'effectuer cette action.';
+ $l['fiche_personnage_desinstallation_button'] = 'Désinstaller le plugin';
+
+ $l['fiche_personnage_error_missing_template'] = 'Il manque un template. Veuillez désactiver puis réactiver le plugin pour corriger ce problème.';
+ $l['fiche_personnage_tablestructure_changed'] = 'La structure de la base de données a été changée. Il serait judicieux d’exporter vos tables concernant les fiches de personnage et de réinstaller le plugin.';
+ $l['fiche_personnage_tablestructure_incomplete'] = 'La base de données ne contient pas les tables nécessaires. Essayez de réinstaller le plugin.';
+
+ ?>
